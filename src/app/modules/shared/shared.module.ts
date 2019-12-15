49abc94fb44da57969a3b9fa36bc0cbbd8d06b43
Szymon Standarski <s.standarski@gmail.com>
2019-12-15 22:52:29 +0100

    feat(*): add header and footer to main page

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,46 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {
+  MatToolbarModule,
+  MatCardModule,
+  MatIconModule,
+  MatButtonModule,
+  MatDividerModule,
+  MatBadgeModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSnackBarModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatMenuModule
+} from "@angular/material";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
+const SHARED_MODULES = [
+  CommonModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatIconModule,
+  MatButtonModule,
+  MatDividerModule,
+  MatBadgeModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSnackBarModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatMenuModule,
+  ReactiveFormsModule,
+  FormsModule,
+  RouterModule
+];
 
+const SHARED_COMPONENTS = [];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [...SHARED_COMPONENTS],
+  imports: [...SHARED_MODULES],
+  exports: [...SHARED_COMPONENTS, ...SHARED_MODULES]
 })
-export class SharedModule { }
+export class SharedModule {}
